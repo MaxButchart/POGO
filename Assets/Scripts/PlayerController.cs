5fc308f6a82fe7565084047394cb6699e3007d50
Max Butchart <max.butchart@live.com>
2020-11-05 22:13:52 +1300

    Controllable Bouncing Bean
    The bean now takes input from the controller to rotate it and bounces in the direction of the rotation. Expanded the ground and created two walls which the bean can bounce off of and climb up.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,6 +6,18 @@ public class PlayerController : MonoBehaviour
 {
     public float bouncePower;
     public Rigidbody rigidbody;
+    
+    public float smooth = 5.0f;
+    public float tiltAngle = 50.0f;
+
+    public float tiltX;
+    public float tiltZ;
+
+    public float vert;
+    public float hori;
+
+    const float upperDeadzone = 0.2f;
+    const float lowerDeadzone = -0.2f;
 
     // Start is called before the first frame update
     void Start()
@@ -17,10 +29,35 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
 
+        vert = Input.GetAxis("Vertical");
+        hori = Input.GetAxis("Horizontal");
+
+        //rotate the player
+        if (Input.GetAxis("Horizontal") > upperDeadzone || Input.GetAxis("Horizontal") < lowerDeadzone)
+        {
+            tiltZ = Input.GetAxis("Horizontal") * tiltAngle;
+        }
+        else
+        {
+            tiltZ = 0f;
+        }
+        if (Input.GetAxis("Vertical") > upperDeadzone || Input.GetAxis("Vertical") < lowerDeadzone)
+        {
+            tiltX = Input.GetAxis("Vertical") * tiltAngle;
+        }
+        else
+        {
+            tiltX = 0f;
+        }
+
+        Quaternion target = Quaternion.Euler(tiltX, 0, tiltZ);
+        // Dampen towards the target rotation
+        transform.rotation = Quaternion.Slerp(transform.rotation, target, Time.deltaTime * smooth);
     }
 
+    //make the player bounce when hitting the ground and move if they are on an angle
     void OnCollisionEnter(Collision collision)
     {
-        rigidbody.velocity = new Vector3(0, bouncePower, 0);
+        rigidbody.velocity = new Vector3(((bouncePower * tiltZ / tiltAngle) * -1), bouncePower, (bouncePower * tiltX / tiltAngle));
     }
 }
