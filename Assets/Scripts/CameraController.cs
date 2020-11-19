e19eac0d191033b99200465cac1b19bb09eb08a7
Max Butchart <max.butchart@live.com>
2020-11-19 21:25:06 +1300

    Charge bounce
    Added vertical adjustment for camera as well as a feature to charge a bounce for more power

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -6,8 +6,16 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
+    const float UPPER_ROTATION_LIMIT = 75f;
+    const float LOWER_ROTATION_LIMIT = -75f;
+
     public Transform bean;
     public Vector3 offset;
+
+    public float desiredAngleY = 0f;
+    public float desiredAngleX = 0f;
+
+    public float rotateSpeed;
 
     // Start is called before the first frame update
     void Start()
@@ -20,9 +28,29 @@ public class CameraController : MonoBehaviour
     {
        
         //move camera based on rotation of the target
-        float desiredAngleY = bean.eulerAngles.y;
+        desiredAngleY = bean.eulerAngles.y;
 
-        Quaternion rotation = Quaternion.Euler(0, desiredAngleY, 0);
+        //move camera vertically based on player input
+        if(desiredAngleX < UPPER_ROTATION_LIMIT && desiredAngleX > LOWER_ROTATION_LIMIT)
+        {
+            desiredAngleX += Input.GetAxis("CameraVertical") * rotateSpeed;
+        }
+        else if (desiredAngleX > UPPER_ROTATION_LIMIT)
+        {
+            if(Input.GetAxis("CameraVertical") < 0f)
+            {
+                desiredAngleX += Input.GetAxis("CameraVertical") * rotateSpeed;
+            }
+        }
+        else if (desiredAngleX < LOWER_ROTATION_LIMIT)
+        {
+            if (Input.GetAxis("CameraVertical") > 0f)
+            {
+                desiredAngleX += Input.GetAxis("CameraVertical") * rotateSpeed;
+            }
+        }
+
+        Quaternion rotation = Quaternion.Euler(desiredAngleX, desiredAngleY, 0);
         transform.position = bean.position - (rotation * offset);
 
         transform.LookAt(bean);
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,8 +6,12 @@ using System;
 public class PlayerController : MonoBehaviour
 {
     const float MAX_VELOCITY = 1;
+    const float MAX_BOUNCE = 0.08f;
 
     public float bouncePower;
+    public float bounceMultiplier;
+    public float charge;
+
     public Rigidbody rigidbody;
     
     public float smooth = 5.0f;
@@ -37,6 +41,20 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //charge jump
+        if (Input.GetAxis("Charge") > 0f)
+        {
+            if (charge < MAX_BOUNCE)
+            {
+                charge += bounceMultiplier * Time.deltaTime;
+            }
+        }
+
+        if (Input.GetAxis("Charge") == 0f)
+        {
+            bouncePower += charge;
+        }
+
         //rotate the player based on the left thumbs
         tiltZ = Input.GetAxis("MoveHorizontal") * tiltAngle * -1f;
         
@@ -52,11 +70,18 @@ public class PlayerController : MonoBehaviour
         //calculate the x and z angle to make the player move in the direction the camera is pointing
         xAngle = bouncePower * ((tiltX / tiltAngle) * (float)Math.Cos(tiltY * Math.PI / 180) + ((tiltZ / tiltAngle) * (float)Math.Sin(tiltY * Math.PI / 180))); 
         zAngle = bouncePower * ((tiltX / tiltAngle) * (float)Math.Sin(tiltY * Math.PI / 180) + ((tiltZ / tiltAngle) * (float)Math.Cos(tiltY * Math.PI / 180) * -1f)); 
+
     }
 
     //make the player bounce when hitting the ground and move if they are on an angle
     void OnCollisionEnter(Collision collision)
     {
         rigidbody.velocity = new Vector3(zAngle, bouncePower,  xAngle);
+        bouncePower = 5f;
+        
+        if (Input.GetAxis("Charge") == 0f)
+        {
+            charge = 0;
+        }
     }
 }
